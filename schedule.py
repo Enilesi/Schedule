c6ad081db0ff1ca1ad97532661ced296f3dca907
Enilesi <miriam.iseline@gmail.com>
2024-10-04 01:14:07 +0300

    init repo

---
+++ schedule.py
@@ -0,0 +1,24 @@
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
+
+class ScheduleCollector:
+    def start(self):
+        options = webdriver.ChromeOptions()
+        
+        with webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options) as driver:
+            driver.get("https://docs.google.com/spreadsheets/d/e/2PACX-1vRXLWDLpnv-108LNY1vMSst-yvrWKqGlytCjlt2Qauid7gv7x2MLzKsO0fPWJ9Cfxj3AfevBvc9gGC0/pubhtml")
+            while True:
+                self.find_schedule(driver)
+
+def main():
+    collector = ScheduleCollector()
+    collector.start()
+
+if __name__ == "__main__":
+    main()
